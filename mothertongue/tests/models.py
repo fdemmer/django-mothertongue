c2fa0eb2ae973fa12b8423c1d630500999ae345e
Florian Demmer <fdemmer@gmail.com>
2013-10-03 20:00:33 +0200

    fix model name

--- mothertongue/tests/models.py
+++ mothertongue/tests/models.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.utils.translation import get_language, ugettext, ugettext_lazy as _
 
 # import translation stuff from mothertongue package
-from mothertongue.models import MothertongueModelTranslate
+from mothertongue.models import TranslatedModelBase
 
 # Create generic test model.
-class TestModel(MothertongueModelTranslate):
+class TestModel(TranslatedModelBase):
     test_field_1 = models.CharField(_('test_field_1'), max_length=200)
     test_field_2 = models.TextField(_('test_field_2'), blank=True)
     translations = models.ManyToManyField('TestModelTranslation', blank=True, verbose_name=_('translations'))
